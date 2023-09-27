02bb0859e88056fa2d51a9035e05c38556abdc80
Vladyslav Gladyr <wytcorporation@gmail.com>
2023-09-27 04:35:13 +0000

    Unit 3

---
+++ my_robot_manager/include/my_robot_manager/robot_manager.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <ros/ros.h>
+
+class RobotManager {
+
+public:
+  std::string robot_name, robot_model;
+  float battery_capacity_kwh = 0.16;
+  int total_operation_hours = 2560;
+  void print_specifications();
+};
